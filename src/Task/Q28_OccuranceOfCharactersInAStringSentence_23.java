8407cc8281c1cc0d3274872d6f53178c61ac8db1
Apurba-Acharya <apurba1907@gmail.com>
2025-07-26 19:11:42 +0530

    26/07

--- src/Task/Q28_OccuranceOfCharactersInAStringSentence_23.java
+++ src/Task/Q28_OccuranceOfCharactersInAStringSentence_23.java
@@ -5,9 +5,9 @@ import java.util.*;
 public class Q28_OccuranceOfCharactersInAStringSentence_23 {
     public static void main(String[] args) {
         String str = "I love coding and I am so happy";
-        String r = str.toLowerCase();
-        String st = r.replaceAll(" ", "");
-        char Char[] = st.toCharArray();
+        String r = str.toLowerCase().replaceAll(" ", "");
+        //String st = r.replaceAll(" ", "");
+        char Char[] = r.toCharArray();
         Map<Character, Integer> hm = new HashMap<>();
         for (char c : Char){
             if (hm.containsKey(c)){
