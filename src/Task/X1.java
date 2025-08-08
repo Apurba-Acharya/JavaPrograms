f0872153de178f6f2b88aa76308d13ae9522d078
Apurba-Acharya <apurba1907@gmail.com>
2025-08-08 08:06:31 +0530

    06/08

--- src/Task/X1.java
+++ src/Task/X1.java
@@ -1,28 +1,33 @@
 package Task;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class X1 {
     public static void main(String[] args) {
-        String st = "seleniums";
-        String str = st.toLowerCase();
+        int inputArr[] = {2,5,6,7,6,8,2,9,9};
+//        Set<Integer> hs = new HashSet();
+//        for(int i : inputArr){
+//            if(!hs.add(i)){
+//                System.out.println(i);
+//            }
+//        }
 
-        char arr[]=str.toCharArray();
-        Map<Character, Integer> map = new HashMap<>();
-        for(char c:arr){
-            if(!map.containsKey(c)){
-                map.put(c, 1);
+        Map<Integer,Integer> hmap = new HashMap();
+        for(int ia:inputArr){
+            if(hmap.containsKey(ia)){
+                hmap.put(ia, hmap.get(ia)+1);
             }else{
-                map.put(c, map.get(c)+1);
+                hmap.put(ia, 1);
             }
         }
-        for(Character key: map.keySet()){
-            if (map.get(key)>1) {
-                System.out.println(key + map.get(key));
+        for (int a : hmap.keySet()){
+            if (hmap.get(a)==1){
+                System.out.println(a);
             }
         }
-
     }
 
 }
